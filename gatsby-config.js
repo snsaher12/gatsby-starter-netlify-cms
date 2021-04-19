5c9b37f087be229a73aa3ead085fb970729949f8
snsaher12 <58394701+snsaher12@users.noreply.github.com>
2021-04-20 00:36:05 +0500

    Add files via upload
    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,48 +1,49 @@
+const path = require("path");
+require("dotenv").config();
+
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    siteURL: process.env.URL,
+    siteUrl: process.env.URL,
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-react-helmet",
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/static/img`,
-        name: 'uploads',
+        name: "uploads",
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
+        name: "pages",
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/src/img`,
-        name: 'images',
+        path: `${__dirname}/src/posts`,
+        name: "posts",
       },
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-relative-images',
+            resolve: "gatsby-remark-relative-images",
             options: {
-              name: 'uploads',
+              name: "uploads",
             },
           },
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
@@ -51,27 +52,54 @@ module.exports = {
             },
           },
           {
-            resolve: 'gatsby-remark-copy-linked-files',
+            resolve: "gatsby-remark-copy-linked-files",
             options: {
-              destinationDir: 'static',
+              destinationDir: "static",
+            },
+          },
+          {
+            resolve: `gatsby-remark-images-native-lazy-load`,
+            options: {
+              loading: "lazy", // "lazy" | "eager" | "auto"
             },
           },
         ],
       },
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: `gatsby-plugin-mdx`,
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
+        gatsbyRemarkPlugins: [{ resolve: `gatsby-remark-auto-headers`, options: { icon: false, elements: [`h2`, `h3`, `h4`] } }],
       },
     },
     {
-      resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
+      resolve: "gatsby-plugin-mdx-frontmatter",
+    },
+    "gatsby-plugin-slug",
+    `gatsby-plugin-remove-fingerprints`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
       options: {
+        exclude: [`/contact-us/thanks`],
+      },
+    },
+    "gatsby-plugin-robots-txt",
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        manualInit: true,
+        modulePath: `${__dirname}/src/cms/cms.js`,
+        customizeWebpackConfig: (config, { plugins }) => ((config.node.fs = "empty"), (config.node.child_process = "empty"), config.plugins.push(plugins.define({ "process.env.MY_BRANCH": JSON.stringify(process.env.BRANCH) }))),
+      },
+    },
+    {
+      resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
+      options: {
+        content: [path.join(process.cwd(), "src/**/!(*.d).{js,mdx,md}")],
         develop: true, // Activates purging in npm run develop
-        purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
+        purgeOnly: ["css/"], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
+    // make sure to keep it last in the array
   ],
-}
+};
