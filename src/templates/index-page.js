94b78e7be2e87f12c19541d0593f6de433b01c01
snsaher12 <58394701+snsaher12@users.noreply.github.com>
2021-04-20 00:45:30 +0500

    Add files via upload
    update

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,193 +1,316 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/Layout";
+import HeadData from "../components/HeadData.js";
+import { Calendar } from "../components/SVG.js";
+import SiteMetaData from "../components/SiteMetadata.js";
+import { FindCategory, FillSpace, LinkFix } from "../components/SimpleFunctions.js";
 
-import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
+const IndexTemplate = ({ data }) => {
+  const singlePost = data.FP.nodes[0];
+  const otherPosts = data.OP.nodes;
+  const allPosts = data.posts.nodes;
+  const sections = data.allMarkdownRemark.categories
+    .map((category) => {
+      const posts = allPosts.filter((post) => post.frontmatter.category === category.frontmatter.id).slice(0, 6);
+      return { title: category.frontmatter.title, posts };
+    })
+    .filter((category) => category.posts.length);
+  const { seoTitle: title, seoDescription: description } = data.markdownRemark.frontmatter;
+  const { siteURL, title: siteName, number, facebook, youtube, twitter, logoLarge } = SiteMetaData();
+  const homeCategories = data.markdownRemark.frontmatter.categories;
 
-export const IndexPageTemplate = ({
-  image,
-  title,
-  heading,
-  subheading,
-  mainpitch,
-  description,
-  intro,
-}) => (
-  <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {title}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {subheading}
-        </h3>
-      </div>
-    </div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </div>
-)
+  const websiteSchema = `{
+      "@context":"https://schema.org",
+      "@type":"WebSite",
+      "@id":"${siteURL}/#website",
+      "headline":"${title}",
+      "name":"${siteName}",
+      "description":"${description}",
+      "url":"${siteURL}",
+      "potentialAction":{
+        "@type":"SearchAction",
+        "target":"${siteURL}/?s={search_term_string}",
+        "query-input":"required name=search_term_string"
+      }
+    }`;
 
-IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
-}
-
-const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  const newsMediaSchema = `{
+        "@context":"https://schema.org",
+        "@type":"NewsMediaOrganization",
+        "@id":"${siteURL}/#Organization",
+        "name":"${siteName}",
+        "url":"${siteURL}",
+        "sameAs":[
+          "${facebook}",
+          "${youtube}",
+          "${twitter}"
+        ],
+        "logo":{
+          "@type":"ImageObject",
+          "url":"${siteURL}/img/${logoLarge.base}",
+          "width":"800",
+          "height":"258"
+        },
+        "contactPoint":{
+          "@type":"ContactPoint",
+          "contactType":"customer support",
+          "telephone":"${number}",
+          "url":"${siteURL}/contact-us/"
+        }
+    }`;
 
   return (
     <Layout>
-      <IndexPageTemplate
-        image={frontmatter.image}
-        title={frontmatter.title}
-        heading={frontmatter.heading}
-        subheading={frontmatter.subheading}
-        mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
-      />
+      <section className="section index">
+        <HeadData title={title} description={description} schema={`[${websiteSchema}, ${newsMediaSchema}]`} />
+        <div className="container content">
+          <div className="index-latest-title">
+            <h2>Latest Posts</h2>
+          </div>
+          <div className="index-top-section">
+            {singlePost && <FirstPost post={singlePost} />}
+            <OtherPosts posts={otherPosts} />
+          </div>
+          <div className="index-bottom-section">
+            <Sections sections={sections} />
+            {!!homeCategories.length && (
+              <div className="index-categories">
+                <div className="index-latest-title">
+                  <h2>Categories</h2>
+                </div>
+                <div className="index-inner-categories">
+                  {homeCategories.map((item, index) => (
+                    <div className="index-category" key={index}>
+                      <h2>{item.title}</h2>
+                      <div className="category-links">
+                        {item.links?.map((item, index) => (
+                          <div className="category-link" key={index}>
+                            <Link to={LinkFix(item)}>{item.title}</Link>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
     </Layout>
-  )
-}
+  );
+};
 
-IndexPage.propTypes = {
+IndexTemplate.propTypes = {
   data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
     }),
   }),
-}
+};
 
-export default IndexPage
+const FirstPost = ({ post }) => {
+  const { title, category, date } = post.frontmatter;
+  const { name: imgName, base: img } = post.frontmatter.featuredimage;
+  const { width, height } = post.frontmatter.featuredimage.childImageSharp.original;
+  const slug = post.fields.slug;
+  const { categoryName, categoryLink } = FindCategory(category);
 
-export const pageQuery = graphql`
-  query IndexPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+  return (
+    <div className="first-post">
+      <div className="first-post-thumbnail">
+        <Link to={`${slug}/`}>
+          <picture>
+            <source media="(min-width:768px)" srcSet={`/image/front-first/${imgName}.webp`} />
+            <source media="(min-width:100px)" srcSet={`/image/post-first/${imgName}-m.webp`} />
+            <img src={`/img/${img}`} alt={title} loading="lazy" width={width} height={height} />
+          </picture>
+        </Link>
+      </div>
+      <div className="first-post-info">
+        <div className="post-info-category">
+          <Link to={`${categoryLink}/`}>{categoryName}</Link>
+        </div>
+        <div className="post-info-title">
+          <Link to={`${slug}/`}>{title}</Link>
+        </div>
+        <div className="post-info-date">
+          <Calendar />
+          {date}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const OtherPosts = ({ posts }) => (
+  <div className="other-posts">
+    {posts.map((post, index) => {
+      const { title, category } = post.frontmatter;
+      const { name: imgName, base: img } = post.frontmatter.featuredimage;
+      const { width, height } = post.frontmatter.featuredimage.childImageSharp.original;
+      const slug = post.fields.slug;
+      const { categoryName, categoryLink } = FindCategory(category);
+
+      return (
+        <div className="other-post" key={index}>
+          <div className="other-post-thumbnail">
+            <Link to={`${slug}/`}>
+              <picture>
+                <source srcSet={`/image/front-right/${imgName}.webp`} />
+                <img src={`/img/${img}`} alt={title} loading="lazy" width={width} height={height} />
+              </picture>
+            </Link>
+          </div>
+          <div className="other-post-info">
+            <div className="post-info-category">
+              <Link to={`${categoryLink}/`}>{categoryName}</Link>
+            </div>
+            <div className="post-info-title">
+              <Link to={`${slug}/`}>{title}</Link>
+            </div>
+            <div className="post-info-date">{post.frontmatter.date}</div>
+          </div>
+        </div>
+      );
+    })}
+  </div>
+);
+
+const Sections = ({ sections }) => {
+  return (
+    <div className="category-sections">
+      {sections.map((category, index) => (
+        <div className="category-section" key={index}>
+          <div className="index-latest-title">
+            <h2>{category.title}</h2>
+          </div>
+          <div className="index-columns">
+            {category.posts.map((post, index) => {
+              const { title } = post.frontmatter;
+              const { name: imgName, base: img } = post.frontmatter.featuredimage;
+              const { width, height } = post.frontmatter.featuredimage.childImageSharp.original;
+              const slug = post.fields.slug;
+
+              return (
+                <div className="index-column" key={index}>
+                  <div className="index-col-image">
+                    <Link to={`${slug}/`}>
+                      <picture>
+                        <source srcSet={`/image/latest/${imgName}.webp`} />
+                        <img src={`/img/${img}`} alt={title} loading="lazy" width={width} height={height} />
+                      </picture>
+                    </Link>
+                  </div>
+                  <div className="index-box-title">
+                    <Link to={`${slug}/`}>{title}</Link>
+                  </div>
+                </div>
+              );
+            })}
+            {FillSpace(category.posts.length)}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const IndexQuery = graphql`
+  query IndexQuery($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
       frontmatter {
-        title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
+        seoTitle
+        seoDescription
+        categories {
+          title
+          links {
+            title
+            link
           }
         }
-        heading
-        subheading
-        mainpitch {
-          title
-          description
+      }
+    }
+    FP: allMdx(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1) {
+      nodes {
+        fields {
+          slug
         }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
+        frontmatter {
+          title
+          date(fromNow: true)
+          category
+          featuredimage {
+            name
+            base
+            childImageSharp {
+              original {
+                height
+                width
               }
             }
-            text
           }
-          heading
-          description
+        }
+      }
+    }
+    OP: allMdx(sort: { order: DESC, fields: [frontmatter___date] }, limit: 3, skip: 1) {
+      nodes {
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+          date(fromNow: true)
+          category
+          featuredimage {
+            name
+            base
+            childImageSharp {
+              original {
+                height
+                width
+              }
+            }
+          }
+        }
+      }
+    }
+    posts: allMdx(sort: { order: DESC, fields: [frontmatter___date] }) {
+      nodes {
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+          category
+          featuredimage {
+            name
+            base
+            childImageSharp {
+              original {
+                height
+                width
+              }
+            }
+          }
+        }
+      }
+    }
+    allMarkdownRemark(filter: { frontmatter: { templateKey: { eq: "category-page" } } }) {
+      categories: nodes {
+        fields {
+          slug
+        }
+        frontmatter {
+          id
+          title
         }
       }
     }
   }
-`
+`;
+
+export default IndexTemplate;
